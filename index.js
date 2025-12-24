a3f419c9fb2a01c99491bf51ec62996fb28e6615
sheraz8141 <sheraz.ahmad8814@gmail.com>
2025-12-24 18:28:55 +0500

    modified commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 module.exports = app;
